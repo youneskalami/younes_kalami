1c5bf369b915137da8c64d83e6d2d738bdcf178c
youneskalami <92359157+youneskalami@users.noreply.github.com>
2022-02-04 10:52:07 +0200

    submitting as exersice3

---
+++ exercise3.php
@@ -0,0 +1,54 @@
+<?php
+
+# SWITCH
+$a = 4;
+switch ($a) {
+    case '0':
+        echo ("the number is 0.");
+        break;
+    case '1':
+        echo ("the number is 1.");
+        break; 
+    case '2':
+        echo ("the number is 2.");
+        break;     
+    case '3':
+        echo ("the number is 3.");
+        break;      
+
+    default:
+        echo ("the number is not in range.");
+        break;
+}
+
+#WHILE 
+
+$b = 0;
+
+while ($b <= 10) {
+    echo "<br>". "the number is ".$b;
+    $b++;
+}
+
+# DO-WHILE
+$c=0;
+do {
+    echo "<br>". "the number is ".$c;
+    $c++;
+    
+} 
+while ($c <= 10);
+
+#FOR LOOP
+for ($i=0; $i <7 ; $i++) { 
+    echo "<br>"."the number is $i <br />";
+}
+
+# FOREACH LOOP
+$x = array(3,5,8,2,12,62,98,32,12);
+foreach ($x as $key => $value) {
+    echo "<br>". $value;
+}
+
+
+?>
